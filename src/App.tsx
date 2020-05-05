980c1d0565b318f7f3aa31901879595bed491ace
Kirill Maksimov <kirill@orbs.com>
2020-05-05 16:01:33 +0300

    Refactor App as a component

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,42 @@ import logo from './logo.svg';
 import './App.css';
 import Connect from "./Connect";
 
-const state = {
-  connected: false,
-};
-
-async function onEthereumEnabled(enabled: boolean) {
-  state.connected = enabled;
+interface AppState {
+  connected: boolean;
 }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Orbs Virtual Chain Console
-      </header>
-      { !state.connected &&
-        <Connect onEthereumEnabled={onEthereumEnabled}/>
-      }
-      {
-        state.connected &&
-        <p>Connected!</p>
-      }
-    </div>
-  );
+class App extends React.Component<{}, AppState> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      connected: false,
+    };
+    
+  }
+  
+  async onEthereumEnabled(enabled: boolean) {
+    this.setState({
+      connected: enabled,
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Orbs Virtual Chain Console
+        </header>
+        { !this.state.connected &&
+          <Connect onEthereumEnabled={(value: boolean) => this.onEthereumEnabled(value)}/>
+        }
+        {
+          this.state.connected &&
+          <p>Connected!</p>
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
