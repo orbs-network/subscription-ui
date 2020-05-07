aa5ec292ebf71c29600da162979a04362fd670c8
Kirill Maksimov <kirill@orbs.com>
2020-05-07 20:59:57 +0300

    Finished confirmation

--- src/Confirmation.tsx
+++ src/Confirmation.tsx
@@ -13,7 +13,11 @@ interface ConfirmationProps {
 
 interface ConfirmationState {
     countdown: number;
-    interval?: any;
+    countdownInterval?: any;
+    confirmationInterval?: any;
+
+    approveTxStatus: string;
+    subscribeTxStatus: string;
 }
 
 class Confirmation extends React.Component<ConfirmationProps, ConfirmationState> {
@@ -22,6 +26,8 @@ class Confirmation extends React.Component<ConfirmationProps, ConfirmationState>
 
         this.state = {
             countdown: 0,
+            approveTxStatus: "pending",
+            subscribeTxStatus: "pending",
         }
     }
 
@@ -30,17 +36,32 @@ class Confirmation extends React.Component<ConfirmationProps, ConfirmationState>
             countdown: 0,
         });
 
-        const interval = setInterval(() => {
+        const countdownInterval = setInterval(() => {
             this.setState({
                 countdown: this.state.countdown + 1,
             });
         }, 1000);
 
-        this.setState({ interval });
+        const confirmationInterval = setInterval(async () => {
+            const approveTxReceipt = await this.props.web3.eth.getTransactionReceipt(this.props.approveTxHash);
+            const subscribeTxReceipt = await this.props.web3.eth.getTransactionReceipt(this.props.subscribeTxHash);
+
+            console.log("approveTxReceipt", approveTxReceipt);
+            console.log("subscribeTxReceipt", subscribeTxReceipt);
+
+            // FIXME stop polling when one of the receipts is not empty
+            this.setState({
+                approveTxStatus: (approveTxReceipt && approveTxReceipt.status) ? "confirmed" : "pending",
+                subscribeTxStatus: (subscribeTxReceipt && subscribeTxReceipt.status) ? "confirmed" : "pending",
+            });
+        }, 3000);
+
+        this.setState({ countdownInterval, confirmationInterval });
     }
 
     componentWillUnmount() {
-        clearInterval(this.state.interval);
+        clearInterval(this.state.countdownInterval);
+        clearInterval(this.state.confirmationInterval);
     }
 
     getEtherscanURL(txHash: string): string {
@@ -49,18 +70,16 @@ class Confirmation extends React.Component<ConfirmationProps, ConfirmationState>
     }
 
     render() {
-        const { countdown } = this.state;
+        const { countdown, approveTxStatus, subscribeTxStatus } = this.state;
         const { approveTxHash, subscribeTxHash } = this.props;
 
         return (
             <div>
                 <Typography paragraph>Waiting for transaction confirmation for {countdown} seconds...</Typography>
 
-                <Typography paragraph>ERC20 approval transaction: 
-                    <a href={this.getEtherscanURL(approveTxHash!)}>{approveTxHash}</a>
+                <Typography paragraph>ERC20 approval <a href={this.getEtherscanURL(approveTxHash!)} target="_blank" className="App-monospace">{approveTxHash}</a> is {approveTxStatus}
                 </Typography>
-                <Typography paragraph>Subscription transaction: 
-                    <a href={this.getEtherscanURL(subscribeTxHash!)}>{subscribeTxHash}</a>
+                <Typography paragraph>Subscription <a href={this.getEtherscanURL(subscribeTxHash!)} target="_blank" className="App-monospace">{subscribeTxHash}</a> is {subscribeTxStatus}
                 </Typography>
             </div>
         );
