3445396534085978bc2db2282d8753dac7e4be0c
Kirill Maksimov <kirill@orbs.com>
2020-05-18 11:30:58 +0300

    Updates

---
+++ precache-manifest.4b08f52308669ec9be08b2ba38fd03b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "451b4c3ecf423649c9b6e4f70fc1c552",
+    "url": "/subscription-ui/index.html"
+  },
+  {
+    "revision": "1859d59d7af02809eacc",
+    "url": "/subscription-ui/static/css/main.ae54e01d.chunk.css"
+  },
+  {
+    "revision": "238588e15d97648ded46",
+    "url": "/subscription-ui/static/js/2.f8f160cf.chunk.js"
+  },
+  {
+    "revision": "91e9bfd1d08880b66e0e6b773fb8e781",
+    "url": "/subscription-ui/static/js/2.f8f160cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1859d59d7af02809eacc",
+    "url": "/subscription-ui/static/js/main.87b92e59.chunk.js"
+  },
+  {
+    "revision": "eb6c3271a403c4de368e",
+    "url": "/subscription-ui/static/js/runtime-main.4919c3b1.js"
+  }
+]);
