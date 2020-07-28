931310815102e044fc29b1de496060b802758727
Or Lavy <orlavyil@gmail.com>
2020-07-28 19:16:49 +0300

    Improves service

--- src/services/monthlySubscriptionPlanService/MonthlySubscriptionPlanService.ts
+++ src/services/monthlySubscriptionPlanService/MonthlySubscriptionPlanService.ts
@@ -3,6 +3,7 @@ import Web3 from "web3";
 import { MonthlySubscriptionPlan } from "../../contracts/MonthlySubscriptionPlan";
 import MonthlySubscriptionPlanContractJson from "@orbs-network/orbs-ethereum-contracts-v2/build/contracts/MonthlySubscriptionPlan.json";
 import { AbiItem } from "web3-utils";
+import { PromiEvent, TransactionReceipt } from "web3-core";
 
 // TODO : O.L : Fill it up after deploying,
 const MAIN_NET_MONTHLY_SUBSCRIPTION_PLAN_CONTRACT_ADDRESS = "";
@@ -25,11 +26,18 @@ export class MonthlySubscriptionPlanService
     amount: number,
     isCertified: boolean,
     deploymentSubset: string
-  ): Promise<boolean> {
-    return Promise.resolve(false);
+  ): PromiEvent<TransactionReceipt> {
+    return this.monthlySubscriptionContract.methods
+      .createVC(amount, isCertified, deploymentSubset)
+      .send();
   }
 
-  extendSubscription(vcid: number, amount: number): Promise<boolean> {
-    return Promise.resolve(false);
+  extendSubscription(
+    vcid: number,
+    amount: number
+  ): PromiEvent<TransactionReceipt> {
+    return this.monthlySubscriptionContract.methods
+      .extendSubscription(vcid, amount)
+      .send();
   }
 }
