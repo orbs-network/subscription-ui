23a9f454f29bb2751055cc65b51514650d66af98
Or Lavy <orlavyil@gmail.com>
2020-07-29 16:55:05 +0300

    Subscription form

--- src/pages/NewVCPage.tsx
+++ src/pages/NewVCPage.tsx
@@ -3,19 +3,28 @@ import { Page } from "../components/structure/Page";
 import configs from "../configs";
 import VirtualChainSubscription from "../VirtualChainSubscription";
 import Web3 from "web3";
+import { ContentFitting } from "../components/structure/ContentFitting";
+import { VirtualChainSubscriptionForm } from "../components/forms/VirtualChainSubscriptionForm";
 
 interface IProps {}
 
 export const NewVCPage = React.memo<IProps>((props) => {
   return (
     <Page>
-      <VirtualChainSubscription
-        web3={{} as Web3}
-        config={configs}
-        virtualChainId="0x0000000000000000000000000000000000000000000000000000000000000001"
-        buttonLabel="Create"
-        subscriptionLabel="Initial subscription"
-      />
+      <ContentFitting>
+        <VirtualChainSubscription
+          web3={{} as Web3}
+          config={configs}
+          virtualChainId="0x0000000000000000000000000000000000000000000000000000000000000001"
+          buttonLabel="Create"
+          subscriptionLabel="Initial subscription"
+        />
+        <VirtualChainSubscriptionForm
+          subscribeNewVC={(virtualChainSubscriptionPayload) =>
+            console.log(virtualChainSubscriptionPayload)
+          }
+        />
+      </ContentFitting>
     </Page>
   );
 });
