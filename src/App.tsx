f16839faf9285dc53c464bb15d1d2565906ed86d
Or Lavy <orlavyil@gmail.com>
2020-09-01 17:36:28 +0200

    Changes 'App' to a functional component

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,21 @@
 import React from "react";
 import "./App.css";
-import Connect from "./Connect";
-import Web3 from "web3";
 
-import { withStyles } from "@material-ui/core/styles";
-import Drawer from "@material-ui/core/Drawer";
-import AppBar from "@material-ui/core/AppBar";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Toolbar from "@material-ui/core/Toolbar";
-import List from "@material-ui/core/List";
-import Typography from "@material-ui/core/Typography";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import VirtualChainSubscription from "./VirtualChainSubscription";
-import ExistingVirtualChain from "./ExistingVirtualChain";
-import configs from "./configs";
-import { config } from "react-spring";
+import { makeStyles } from "@material-ui/core/styles";
 import { Header } from "./components/structure/header/Header";
 import { HEADER_HEIGHT_REM } from "./theme/Theme";
 import { Route, RouteProps, Switch } from "react-router-dom";
-import { HomePage } from "./pages/HomePage";
 import { NewVCPage } from "./pages/NewVCPage";
 import { ExistingVCPage } from "./pages/ExistingVCPage";
 import { RecoverVCPage } from "./pages/RecoverVcPage";
 import { ROUTES } from "./constants/routes";
 import { VcCreationSuccessPage } from "./pages/VcCreationSuccessPage";
 import { VcExtensionSuccessPage } from "./pages/VcExtensionSuccessPage";
-
-interface AppState {
-  connected: boolean;
-  web3?: Web3;
-
-  newVirtualChain: boolean;
-  existingVirtualChain: boolean;
-}
+import { observer } from "mobx-react";
 
 const drawerWidth = 240;
 
-const useStyles = withStyles((theme) => ({
+const useStyles = makeStyles((theme) => ({
   appMain: {
     height: "100%",
     maxWidth: "90%",
@@ -77,61 +55,33 @@ const useStyles = withStyles((theme) => ({
   },
 }));
 
-// TODO : O.L : Make this a function.
+const App = observer(() => {
+  const classes = useStyles();
+  return (
+    <>
+      <Header />
+      <div className={classes.headerSeparator} />
+      <div className={classes.mainWrapper}>
+        <main className={classes.appMain}>
+          <Switch>
+            <Route exact path={ROUTES.newVc} component={NewVCPage} />
+            <Route exact path={ROUTES.existingVc} component={ExistingVCPage} />
+            <Route exact path={ROUTES.recoverVc} component={RecoverVCPage} />
+            <Route
+              path={`${ROUTES.vcCreated}/:vcId`}
+              component={VcCreationSuccessPage}
+            />
+            <Route
+              path={`${ROUTES.vcExtended}/:vcId`}
+              component={VcExtensionSuccessPage}
+            />
+            <Route path="/" component={NewVCPage} />
+          </Switch>
+          {/*)}*/}
+        </main>
+      </div>
+    </>
+  );
+});
 
-class App extends React.Component<{}, AppState> {
-  constructor(props: any) {
-    super(props);
-
-    this.state = {
-      connected: false,
-      newVirtualChain: false,
-      existingVirtualChain: false,
-    };
-  }
-
-  async onEthereumEnabled(connected: boolean) {
-    const web3 = new Web3((window as any).ethereum);
-
-    this.setState({
-      connected,
-      web3,
-    });
-  }
-
-  render() {
-    const { classes } = this.props as any;
-
-    return (
-      <>
-        <Header />
-        <div className={classes.headerSeparator} />
-        <div className={classes.mainWrapper}>
-          <main className={classes.appMain}>
-            <Switch>
-              <Route exact path={ROUTES.newVc} component={NewVCPage} />
-              <Route
-                exact
-                path={ROUTES.existingVc}
-                component={ExistingVCPage}
-              />
-              <Route exact path={ROUTES.recoverVc} component={RecoverVCPage} />
-              <Route
-                path={`${ROUTES.vcCreated}/:vcId`}
-                component={VcCreationSuccessPage}
-              />
-              <Route
-                path={`${ROUTES.vcExtended}/:vcId`}
-                component={VcExtensionSuccessPage}
-              />
-              <Route path="/" component={NewVCPage} />
-            </Switch>
-            {/*)}*/}
-          </main>
-        </div>
-      </>
-    );
-  }
-}
-
-export default useStyles(App);
+export default App;
