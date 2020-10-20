79e703bdd637bfe0baf20d06d484ceaa4432f1ed
Or Lavy <orlavyil@gmail.com>
2020-10-20 15:18:31 +0300

    Adds back link to revocerVcPage

--- src/components/structure/header/links.ts
+++ src/components/structure/header/links.ts
@@ -3,7 +3,7 @@ import { useMemo } from "react";
 const links = [
   { translationKey: "New Virtual Chain", url: "/new_vc" },
   { translationKey: "Existing Virtual Chain", url: "/existing_vc" },
-  // { translationKey: "Recover Virtual Chain", url: "/recover_vc" },
+  { translationKey: "Recover Virtual Chain", url: "/recover_vc" },
 ];
 
 export type TLinkDescriptor = {
