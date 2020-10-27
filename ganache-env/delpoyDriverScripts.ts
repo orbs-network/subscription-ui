11e4ce229f10b4115e9dd2cfb263bb5ac16452e2
Or Lavy <orlavyil@gmail.com>
2020-10-27 17:17:11 +0200

    Adds vc creation to deplyo script

--- ganache-env/delpoyDriverScripts.ts
+++ ganache-env/delpoyDriverScripts.ts
@@ -7,6 +7,7 @@ import { Driver as OrbsV2Driver } from "@orbs-network/orbs-ethereum-contracts-v2
 // import { Driver as OrbsV2Driver } from "@orbs-network/orbs-ethereum-contracts-v2/build/contracts/Rewards.json";
 import fs from "fs";
 import { weiOrbsFromFullOrbs } from "../src/cryptoUtils/unitConverter";
+import { TransactionConfig } from "web3-core";
 
 const co = require("@orbs-network/orbs-ethereum-contracts-v2");
 const BN = require("bn.js");
@@ -21,13 +22,94 @@ const deployDriverScripts = async () => {
     console.log(`Assigning ORBS to ${orbsV2Account}`);
     driver.erc20.assign(orbsV2Account, new BN("1000000000000000000000000000"));
 
-    await driver.protocol.createDeploymentSubset("canary", 1, {
+    const CANARY_SUBSET = "canary";
+
+    await driver.protocol.createDeploymentSubset(CANARY_SUBSET, 1, {
       from: driver.functionalManager.address,
     });
 
+    const priceOfOneMonthInOrbs = 4000;
+    // const priceOfOneMonthInWeiBn = BN(weiOrbsFromFullOrbs(4000));
+
     const monthlySubscriptionPlanDeployedInstance = await driver.newSubscriber(
       "my_tier",
-      new BN(weiOrbsFromFullOrbs(4000))
+      new BN(weiOrbsFromFullOrbs(priceOfOneMonthInOrbs))
+    );
+
+    const monthsForVc1 = 3;
+    const monthsForVc2 = 1;
+    const monthsForVc3 = 6;
+    const monthsForVc4 = 3;
+    const monthsForVc5 = 1;
+
+    const totalMonths =
+      monthsForVc1 + monthsForVc2 + monthsForVc3 + monthsForVc4 + monthsForVc5;
+    const totalAllowanceInOrbs = totalMonths * priceOfOneMonthInOrbs;
+    const totalAllowanceInWeiOrbs = toWei(new BN(totalAllowanceInOrbs));
+
+    console.log(`Approving usage of ${totalAllowanceInOrbs} for VC creation`);
+    await driver.erc20.approve(
+      monthlySubscriptionPlanDeployedInstance.address,
+      totalAllowanceInWeiOrbs,
+      {
+        from: orbsV2Account,
+      }
+    );
+
+    const weiAmount = weiOrbsFromFullOrbs(2000);
+    console.log({ weiAmount });
+    const bnAmount = new BN(weiAmount);
+    console.log({ bnAmount });
+    console.log(`Creating VC 1`);
+    console.log(weiOrbsFromFullOrbs(priceOfOneMonthInOrbs * monthsForVc1));
+    await monthlySubscriptionPlanDeployedInstance.createVC(
+      "Vc1",
+      new BN(weiOrbsFromFullOrbs(priceOfOneMonthInOrbs * monthsForVc1)),
+      true,
+      CANARY_SUBSET,
+      {
+        from: orbsV2Account,
+      }
+    );
+    console.log(`Creating VC 2`);
+    await monthlySubscriptionPlanDeployedInstance.createVC(
+      "Vc2",
+      new BN(weiOrbsFromFullOrbs(priceOfOneMonthInOrbs * monthsForVc2)),
+      true,
+      CANARY_SUBSET,
+      {
+        from: orbsV2Account,
+      }
+    );
+    console.log(`Creating VC 3`);
+    await monthlySubscriptionPlanDeployedInstance.createVC(
+      "Vc3",
+      new BN(weiOrbsFromFullOrbs(priceOfOneMonthInOrbs * monthsForVc3)),
+      true,
+      CANARY_SUBSET,
+      {
+        from: orbsV2Account,
+      }
+    );
+    console.log(`Creating VC 4`);
+    await monthlySubscriptionPlanDeployedInstance.createVC(
+      "Vc4",
+      new BN(weiOrbsFromFullOrbs(priceOfOneMonthInOrbs * monthsForVc4)),
+      true,
+      CANARY_SUBSET,
+      {
+        from: orbsV2Account,
+      }
+    );
+    console.log(`Creating VC 5`);
+    await monthlySubscriptionPlanDeployedInstance.createVC(
+      "Vc5",
+      new BN(weiOrbsFromFullOrbs(priceOfOneMonthInOrbs * monthsForVc5)),
+      true,
+      CANARY_SUBSET,
+      {
+        from: orbsV2Account,
+      }
     );
 
     const addresses = {
